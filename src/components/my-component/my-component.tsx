dd92039af90c6e5a92bdece4fa5af55110ddb2e3
Sergio Serrano <sdserranog@gmail.com>
2020-04-02 15:58:26 -0500

    import design-pichincha

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,11 +1,8 @@
-import { Component, Prop, h } from '@stencil/core';
-import 'design-pichincha'
-import 'design-pichincha/dist/collection/global/spaces.css'
-import 'design-pichincha/dist/collection/global/variables.css'
+import { Component, Prop, h } from "@stencil/core";
 
 @Component({
-  tag: 'my-component',
-  styleUrl: 'my-component.css',
+  tag: "my-component",
+  styleUrls: ["my-component.css"],
   shadow: true
 })
 export class MyComponent {
@@ -24,16 +21,23 @@ export class MyComponent {
    */
   @Prop() last: string;
 
-
-
   render() {
-    return <div>
-    
-    <pichincha-button loading >hola</pichincha-button>
-    <pichincha-button  color='primary' idelement='idButton' disabled>hola</pichincha-button>
-    <pichincha-spinner size={10} border={4} class='bp-m-20' ></pichincha-spinner>
-    <pichincha-typography variant='h1' color='blue' align='center'> holagg</pichincha-typography>
-  
-    </div>;
+    return (
+      <div>
+        <pichincha-button loading>hola</pichincha-button>
+        <pichincha-button color="primary" idelement="idButton" disabled>
+          hola
+        </pichincha-button>
+        <pichincha-spinner
+          size={10}
+          border={4}
+          class="bp-m-20"
+        ></pichincha-spinner>
+        <pichincha-typography variant="h1" color="blue" align="center">
+          {" "}
+          holagg
+        </pichincha-typography>
+      </div>
+    );
   }
 }
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,24 +1,14 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 
 export const config: Config = {
-  namespace: 'wc-digimoney',
+  namespace: "wc-digimoney",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-      copy: [
-        { src: '../node_modules/design-pichincha/dist/collection/global/variables.css' },
-        { src: '../node_modules/design-pichincha/dist/collection/global/spaces.css' },
-      ],
+      type: "docs-readme"
     },
     {
-      type: 'docs-readme'
-    },
-    {
-      type: 'www',
+      type: "www",
       serviceWorker: null // disable service workers
     }
-  ],
-
-  globalStyle: 'node_modules/design-pichincha/dist/collection/global/variables.css'
-}
+  ]
+};
